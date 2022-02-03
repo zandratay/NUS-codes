12ae9b4079cf770b55f6fb9b15f9384ce0c74d5d
zandratay <88842830+zandratay@users.noreply.github.com>
2022-02-03 20:57:33 +0800

    Create fizzbuzz.java

---
+++ Java/fizzbuzz.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class fizzbuzz {
+
+	public static void main (String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int n = sc.nextInt();
+
+		for (int i = 1; i < n + 1; i++) {
+			if (i % x == 0 && i % y == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % x == 0) {
+				System.out.println("Fizz");
+			} else if (i % y == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}

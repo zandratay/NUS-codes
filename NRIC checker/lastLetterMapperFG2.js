52dcda79d9da46e915ebf1d777d060f0ebf4fd5b
zandratay <88842830+zandratay@users.noreply.github.com>
2023-01-10 09:59:17 +0800

    Create lastLetterMapperFG2.js

---
+++ NRIC checker/lastLetterMapperFG2.js
@@ -0,0 +1,20 @@
+function lastLetterMapperFG2(rem) {
+
+  var dict = {
+    0 : 'X',
+    1 : 'W',
+    2 : 'U',
+    3 : 'T',
+    4 : 'R',
+    5 : 'Q',
+    6 : 'P',
+    7 : 'N',
+    8 : 'M',
+    9 : 'L',
+    10 : 'K'
+  };
+
+  return dict[rem];
+
+// if F or G
+}

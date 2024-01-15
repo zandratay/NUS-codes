60f4f5e9c4c43e4904000965e4ed93ab4d81fa6b
zandratay <88842830+zandratay@users.noreply.github.com>
2024-01-15 21:13:28 +0800

    Delete Hackerrank/RunnerUp.py

--- Hackerrank/RunnerUp.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    arr = list(arr)
-    
-    new_list = [int(i) for i in arr]
-    new_list.sort()
-    new_list = list( dict.fromkeys(new_list) )
-    print(new_list[len(new_list) - 2])
